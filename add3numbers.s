2766001443fdc34325770c25cc23707f27cad2d3
Surya Duraivenkatesh <surya.duraivenkatesh@gmail.com>
2025-03-01 08:00:21 -0800

    Compiled my tester file on my mac (need to use Replit).

---
+++ add3numbers.s
@@ -0,0 +1,43 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 14, 4	sdk_version 14, 4
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #32
+	.cfi_def_cfa_offset 32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	add	x29, sp, #16
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #6
+	str	x8, [sp]
+Lloh0:
+	adrp	x0, l_.str@PAGE
+Lloh1:
+	add	x0, x0, l_.str@PAGEOFF
+	bl	_printf
+	mov	w0, #0
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	add	sp, sp, #32
+	ret
+	.loh AdrpAdd	Lloh0, Lloh1
+	.cfi_endproc
+                                        ; -- End function
+	.globl	_add3                           ; -- Begin function add3
+	.p2align	2
+_add3:                                  ; @add3
+	.cfi_startproc
+; %bb.0:
+	add	w8, w1, w0
+	add	w0, w8, w2
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"%d"
+
+.subsections_via_symbols
