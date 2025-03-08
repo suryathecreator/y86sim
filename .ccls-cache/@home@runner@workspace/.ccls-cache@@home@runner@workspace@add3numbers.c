aae130a1d6be52637f0893aacc9b568e00d43b1a
Surya Duraivenkatesh <surya.duraivenkatesh@gmail.com>
2025-03-08 18:16:42 +0000

    Finished up the assembly file reading logic.

---
+++ .ccls-cache/@home@runner@workspace/.ccls-cache@@home@runner@workspace@add3numbers.c
@@ -0,0 +1,19 @@
+/*
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <math.h>
+ 
+ int add3(int, int, int);
+
+ 
+ // Testing function
+ int main() {
+    int a = add3(1,2,3);
+ }
+
+int add3(int x, int y, int z) {
+    return x + y + z;
+}
+
--- add3numbers.s
+++ add3numbers.s
@@ -1,43 +1,25 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 14, 4	sdk_version 14, 4
-	.globl	_main                           ; -- Begin function main
-	.p2align	2
-_main:                                  ; @main
+	.file	"add3numbers.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB22:
 	.cfi_startproc
-; %bb.0:
-	sub	sp, sp, #32
-	.cfi_def_cfa_offset 32
-	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
-	add	x29, sp, #16
-	.cfi_def_cfa w29, 16
-	.cfi_offset w30, -8
-	.cfi_offset w29, -16
-	mov	w8, #6
-	str	x8, [sp]
-Lloh0:
-	adrp	x0, l_.str@PAGE
-Lloh1:
-	add	x0, x0, l_.str@PAGEOFF
-	bl	_printf
-	mov	w0, #0
-	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
-	add	sp, sp, #32
-	ret
-	.loh AdrpAdd	Lloh0, Lloh1
-	.cfi_endproc
-                                        ; -- End function
-	.globl	_add3                           ; -- Begin function add3
-	.p2align	2
-_add3:                                  ; @add3
-	.cfi_startproc
-; %bb.0:
-	add	w8, w1, w0
-	add	w0, w8, w2
+	movl	$0, %eax
 	ret
 	.cfi_endproc
-                                        ; -- End function
-	.section	__TEXT,__cstring,cstring_literals
-l_.str:                                 ; @.str
-	.asciz	"%d"
-
-.subsections_via_symbols
+.LFE22:
+	.size	main, .-main
+	.globl	add3
+	.type	add3, @function
+add3:
+.LFB23:
+	.cfi_startproc
+	addl	%esi, %edi
+	leal	(%rdi,%rdx), %eax
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	add3, .-add3
+	.ident	"GCC: (GNU) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
