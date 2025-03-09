9665a16289a6e1c084fb9b7a77fb1b6150324776
Surya Duraivenkatesh <surya.duraivenkatesh@gmail.com>
2025-03-09 20:00:54 +0000

    Typed up the testing files (asum.s and asum.o). asum.o is the desired result from our emulator, so we will see if our emulator can produce that from asum.s.

---
+++ asum.s
@@ -0,0 +1,35 @@
+init: irmovl Stack, %esp     # Set up stack pointer
+      irmovl Stack, %ebp     # Set up base pointer
+      call Main               # Execute main program
+      halt                    # Terminate program
+array: .long 0xd
+       .long 0xc0
+       .long 0xb00
+       .long 0xa000
+Main:  pushl %ebp
+       rrmovl %esp, %ebp
+       irmovl $4, %eax
+       pushl %eax             # Push 4
+       irmovl array, %edx
+       pushl %edx             # Push array
+       call Sum               # Sum(array, 4)
+       rrmovl %ebp, %esp
+       popl %ebp
+       ret
+Sum:   pushl %ebp
+       rrmovl %esp, %ebp
+       mrmovl 8(%ebp), %ecx   # ecx = Start
+       mrmovl 12(%ebp), %edx  # edx = Count
+       xorl %eax, %eax        # sum = 0
+       andl %edx, %edx        # Set condition codes
+       je End
+Loop:  mrmovl (%ecx), %esi    # get *Start
+       addl %esi, %eax        # add to sum
+       irmovl $4, %ebx
+       addl %ebx, %ecx        # Start++
+       irmovl $-1, %ebx
+       addl %ebx, %edx        # Count--
+       jne Loop               # Stop when 0
+End:   rrmovl %ebp, %esp
+       popl %ebp
+       ret
